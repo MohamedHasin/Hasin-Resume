8ef21d16ba19a1fc30ddd21f5407d82af00f1541
MohamedHasin <72894270+MohamedHasin@users.noreply.github.com>
2022-06-26 19:04:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//progress bar of languages spoken, will start once the page is loaded/refreshed
+{var i = 0;
+window.onload=function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("Languages");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+}
+
+// alert function - greeting
+{function hi() {
+  alert('Hi! I am Hasin and this is my resume. This resume contains mostly dummy information. You can print or download the resume by clicking the "Print" button below. Enjoy reading and have a great day! :D');
+}
+setTimeout(hi, 1100);
+
+}
+
